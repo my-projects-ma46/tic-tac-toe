0dd27b6635ab302ff494da17a8a6365c2e4d62d4
santosm46 <santos46ma@gmail.com>
2019-05-12 21:30:33 -0300

    Player vs Player game finished

--- src/interface.py
+++ src/interface.py
@@ -7,16 +7,24 @@ CIRCLE = 2
 WIN = 3
 LOSE = 4
 
+BTTN_SIZE = 1
+
 image = {
     CROSS:'X',
     CIRCLE:'O',
-    BLANK:'.',
-    WIN:'win',
-    LOSE:'lose'
+    BLANK:'_',
+    WIN:'',
+    LOSE:''
+}
+
+line = {
+    'r':'linha',
+    'c':'coluna',
+    'a':'diagonal cresc',
+    'd':'diagonal decresc'
 }
 
 startingTurnOf = CROSS
-
 
 def updateGridFrame():
     pass
@@ -34,73 +42,63 @@ class GridState:
     
     def setRowCol(self, row, col, symbol):
         self.grid[row*3+col] = symbol
-        # fix debug
-        print("novo estado do grid ",self.grid)
     
     def getRowCol(self, row, col):
         return self.grid[row*3+col]
 
 class Bttn:
-    def __init__(self, i, j, currentGridState, gridFrame, turnOf, game): # turnOf
+    def __init__(self, i, j, game):
         self.i = i
         self.j = j
         
         self.bttnImageTxt = tk.StringVar()
         
-        self.buttonState = currentGridState.getRowCol(i, j)
+        self.buttonState = game.currentGridState.getRowCol(i, j)
         
-        self.bttn = tk.Button(gridFrame, textvariable=self.bttnImageTxt,
-         command=lambda: self.markButton(currentGridState, game))
+        self.bttn = tk.Button(game.gridFrame, textvariable=self.bttnImageTxt,
+         font=("Helvetica",30), fg='black', height = BTTN_SIZE, width = BTTN_SIZE, command=lambda: self.markButton(game))
         
         self.bttn.grid(row=self.i,column=self.j)
     
-    def markButton(self, currentGridState, game):
-        temp = currentGridState.getRowCol(self.i, self.j)
+    def markButton(self, game):
+        temp = game.currentGridState.getRowCol(self.i, self.j)
         if((temp != CROSS) and (temp != CIRCLE)):
             self.buttonState = game.turnOf
-            currentGridState.setRowCol(self.i, self.j, self.buttonState)
+            game.markedCells += 1
+            self.bttn.config(state=tk.DISABLED)
+            game.currentGridState.setRowCol(self.i, self.j, self.buttonState)
             self.bttnImageTxt.set(image[self.buttonState])
-            #self.bttnImageTxt = image[turnOf]
-        else:
-            # fix tirar isso depois
-            self.buttonState = BLANK
-            currentGridState.setRowCol(self.i, self.j, self.buttonState)
-            self.bttnImageTxt.set(image[self.buttonState])
-
-
-
-
+            #=== check if someone won
+            if(game.isFinished()):
+                if(game.winnerLine[1] == 'n'):
+                    #no one won
+                    game.outputText.set("Ninguém ganhou!")
+                else:
+                    numline = ''
+                    if(game.winnerLine[0] != ' '):
+                        numline = str(int(game.winnerLine[0])+1)
+                    for b in game.buttons.buttons:
+                        b.bttn.config(state=tk.DISABLED)
+                    game.outputText.set("jogador {} ganhou na {} {}".format(
+                        image[game.winner], line[game.winnerLine[1]], numline)
+                    )
+            else:
+                game.switchTurn()
 
 class GridButtons:
-    def __init__(self, currentGridState, gridFrame, turnOf, game):
+    def __init__(self, game):
         
         self.buttons = []
 
         for i in range(3):
             for j in range(3):
-                #try:
                 self.buttons.append(
-                    Bttn(i, j, currentGridState, gridFrame, turnOf, game)
+                    Bttn(i, j, game)
                 )
-                #except:
-                #    print("n deu pra criar botão {}, i={}, j={}".format(i*3+j,i,j))
-    
-    def getButtonState(self, i, j):
-        # fix
-        tent = -1
-        try:
-            tent = self.buttons[i*3+j].buttonState
-        except:
-            print("[i*3+j] deu {}, i={}, j={}".format(i*3+j,i,j))
-            print("estado ",end='')
-            print(tent)
-        return tent
-    
-    '''def setButton(self, i, j, newButtonState):
-        # fix isso funciona?
-        self.buttons[i*3+j].buttonState = newButtonState'''
-    
 
+    def getButtonState(self, i, j):
+        return self.buttons[i*3+j].buttonState
+    
 class Game:
     def __init__(self, root, turnOf=None, state=None):
         if state is None:
@@ -113,26 +111,18 @@ class Game:
         else:
             self.turnOf = turnOf
         
-
         self.winner = BLANK
         self.winnerLine = "--"
-
-        
+        self.markedCells = 0
 
         self.gridFrame = tk.Frame(root)
         self.outputText = tk.StringVar()
         self.output = tk.Label(root, textvariable=self.outputText)
-        
-        self.buttons = GridButtons(self.currentGridState, self.gridFrame, self.turnOf, self)
-        
-        self.output.grid(row=1,column=0)
-        self.gridFrame.grid(row=2,column=0)
+        self.buttons = GridButtons(self)
+
+        self.output.grid(row=1,column=1)
+        self.gridFrame.grid(row=2,column=1,sticky=tk.W+tk.E+tk.N+tk.S)
         self.updateTurnOfTxt()
-
-        while(not self.isFinished()):
-            pass
-            # fix lupi
-
 
     def updateTurnOfTxt(self):
         self.outputText.set("Vez de {}".format(image[self.turnOf]))
@@ -144,56 +134,91 @@ class Game:
             self.turnOf = CROSS
         self.updateTurnOfTxt()
     
-    #def markButton(self, i,j):
-    #    self.buttons[]
-    
     def isFinished(self):
-        
-        finished = True
+        tempWinner = BLANK
+
+
+        # checking rows
         for i in range(3):
-            self.winner = self.buttons.getButtonState(i, 0)
+            finished = True
+            tempWinner = self.buttons.getButtonState(i, 0)
             for j in range(3):
-                finished = finished and (self.winner == self.buttons.getButtonState(i, j))
-                # fix break quebra tudo?
-                if(not finished):
+                if(tempWinner != self.buttons.getButtonState(i, j)):
+                    finished = False
                     break
-            if(finished):
+            if(finished and ((tempWinner == CROSS) or (tempWinner == CIRCLE))):
+                self.winner = tempWinner
                 self.winnerLine = "{}r".format(i)
                 return True
         
+        #checking columns
         for j in range(3):
-            self.winner = self.buttons.getButtonState(0, j)
+            finished = True
+            tempWinner = self.buttons.getButtonState(0, j)
             for i in range(3):
-                finished = finished and (self.winner == self.buttons.getButtonState(i, j))
-                # fix break quebra tudo?
-                if(not finished):
+                if(tempWinner != self.buttons.getButtonState(i, j)):
+                    finished = False
                     break
-            if(finished):
-                self.winnerLine = "{}c".format(i)
+            if(finished and ((tempWinner == CROSS) or (tempWinner == CIRCLE))):
+                self.winner = tempWinner
+                self.winnerLine = "{}c".format(j)
                 return True
+        
+        #checking ascending diagonal
+        finished = True
+        tempWinner = self.buttons.getButtonState(2, 0)
+        for i in range(3):
+                if(tempWinner != self.buttons.getButtonState(2-i, i)):
+                    finished = False
+                    break
+        if(finished and ((tempWinner == CROSS) or (tempWinner == CIRCLE))):
+            self.winner = tempWinner
+            self.winnerLine = " a"
+            return True
 
+        #checking descending diagonal
+        finished = True
+        tempWinner = self.buttons.getButtonState(0, 0)
+        for i in range(3):
+                if(tempWinner != self.buttons.getButtonState(i, i)):
+                    finished = False
+                    break
+        if(finished and ((tempWinner == CROSS) or (tempWinner == CIRCLE))):
+            self.winner = tempWinner
+            self.winnerLine = " d"
+            return True
+
+        # checking if no one won
+        if(self.markedCells == 9):
+            self.winner = BLANK
+            self.winnerLine = "nn"
+            return True
+        
+        # in case the game isn't finished
         self.winner = BLANK
         self.winnerLine = "--"
         return False
-        
+    
+def reset(root):
+    root.destroy()
+    run()
 
 def run():
     os.system("clear")
-
+    width, height = 400, 300
     root = tk.Tk()
     root.title("Jogo da Velha")
-    root.geometry("400x400+200+200")
+    root.geometry("{}x{}+600+300".format(width, height))
     
     optionsFrame = tk.Frame(root)
-    
     optionsFrame.grid(row=0,column=0)
     
     g = Game(root)
 
-    change = tk.Button(optionsFrame, text='mudar vez',
-      command=lambda: g.switchTurn())
-
+    change = tk.Button(optionsFrame, text='Nova partida',
+      command=lambda: reset(root))
     change.grid()
+
     root.mainloop()
 
 if(__name__ == '__main__'):
